951693526c6f90265c7748c356dda3291338eee8
Yasin Neyestani <neyestaniyasin@gmail.com>
2025-09-11 20:30:17 +0330

    main files

---
+++ CV.js
@@ -0,0 +1,17 @@
+const imagePaths = [
+  "./image/album4.jpg",
+  "./image/album1.jpg",
+  "./image/album.jpg",
+  "./image/album2.jpg",
+  "./image/album3.jpg"
+];
+
+let currentIndex = 0;
+const imageElement = document.getElementById("album-image");
+
+function changeImage() {
+  currentIndex = (currentIndex + 1) % imagePaths.length;
+  imageElement.src = imagePaths[currentIndex];
+}
+
+setInterval(changeImage, 4000); // هر 40 ثانیه یک‌بار
